09fae971cdddfd38e029a59be26d34fd79818ce9
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2022-01-07 22:14:55 +0100

    Update from RAE

---
+++ README.md
@@ -0,0 +1,7 @@
+# List of all word according to RAE
+
+Work explained here:
+https://duenaslerin.com/diccionario-palabras-espanol-en-texto-script/
+
+
+Updated with RAE server in: 2022-01-07
--- src/spliter.sh
+++ src/spliter.sh
@@ -2,11 +2,11 @@
 
 LETRAS=('a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n' 'ñ' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z')
 
-cat palabras_todas.txt | grep -v '.*-$' | grep -v ^- | sort | uniq > docs/0_palabras_todas.txt
+cat palabras_todas.txt | grep -v '.*-$' | grep -v ^- | sort | uniq > 0_palabras_todas.txt
 
-cat palabras_todas.txt | grep '.*-$' | sort | uniq > docs/0_prefijos.txt
-cat palabras_todas.txt | grep ^- | sort | uniq > docs/0_subfijos.txt
+cat palabras_todas.txt | grep '.*-$' | sort | uniq > 0_prefijos.txt
+cat palabras_todas.txt | grep ^- | sort | uniq > 0_subfijos.txt
 
 for l in "${LETRAS[@]}"; do
-  cat docs/0_palabras_todas.txt | grep ^$l > docs/palabras_$l.txt
+  cat 0_palabras_todas.txt | grep ^$l > palabras_$l.txt
 done
