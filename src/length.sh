223393585776da661cd821d79259fc54bd50193a
hasecilu <hasecilu@protonmail.com>
2022-01-30 08:27:29 -0600

    código mejorado

--- src/length.sh
+++ src/length.sh
@@ -2,28 +2,30 @@
 
 # By: @hasecilu
 
-# Run this script inside the "diccionario-espanol-txt/src" folder
+if [ ${PWD##*/} = "diccionario-espanol-txt" ]
+then
+	mkdir -pv length
 
-cd ..
-mkdir -pv length
+	FILENAME="0_palabras_todas.txt"
+	LINES=$(cat $FILENAME)
 
-FILENAME="0_palabras_todas.txt"
-LINES=$(cat $FILENAME)
+	COUNTER=0
 
-COUNTER=0
-
-for LINE in $LINES
-do
-	if [ ${#LINE} -lt 10 ]
-	then
-		echo $LINE >> ./length/0${#LINE}.txt
-	else
-		echo $LINE >> ./length/${#LINE}.txt		# time: 7.01s user 2.54s system 99% cpu 9.595     total
-		# echo $LINE | tee -a ./length/${#LINE}.txt	# time: 1263.37s user 3400.90s system 131% cpu 58:60.00 total
-	fi
+	for LINE in $LINES
+	do
+		if [ ${#LINE} -lt 10 ]
+		then
+			echo $LINE >> ./length/0${#LINE}.txt
+		else
+			echo $LINE >> ./length/${#LINE}.txt		# time:    7.01s user    2.54s system  99% cpu     9.595 total
+			# echo $LINE | tee -a ./length/${#LINE}.txt	# time: 1263.37s user 3400.90s system 131% cpu 58:60.00  total
+		fi
+		
+		COUNTER=$((COUNTER+1))
+	done
 	
-	COUNTER=$((COUNTER+1))
-done
-
-wc -l ./length/*
+	wc -l ./length/*
+else
+	echo "Go to the diccioanrio-espanol-folder"
+fi
 
--- src/starting_letter.sh
+++ src/starting_letter.sh
@@ -2,16 +2,17 @@
 
 # By: @hasecilu, some lines were extracted from spliter.sh file
 
-# Run this script inside the "diccionario-espanol-txt/src" folder
+if [ ${PWD##*/} = "diccionario-espanol-txt" ]
+then
+	mkdir -pv starting_letter
+	LETRAS=('a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n' 'ñ' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z')
 
-cd ..
-mkdir -pv starting_letter
+	for l in "${LETRAS[@]}"; do
+		cat 0_palabras_todas.txt | grep ^$l > starting_letter/$l.txt
+	done
 
-LETRAS=('a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n' 'ñ' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z')
-
-for l in "${LETRAS[@]}"; do
-  cat 0_palabras_todas.txt | grep ^$l > starting_letter/$l.txt
-done
-
-wc -l starting_letter/*
+	wc -l starting_letter/*
+else
+	echo "Go to the diccionario-espanol-txt folder"
+fi
 
